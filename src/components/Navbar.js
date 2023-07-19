4ff34667f7a7190fe8a6494df064470f091729c5
Cristina Marian <cristinamarian34@gmail.com>
2023-07-19 19:49:56 +0300

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,16 +12,16 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img src ={Logo} alt="logo pizza"/>
         <div className="hiddenLinks">
-          <Link to="/"> Home </Link>
+          <Link to="/home"> Home </Link>
           <Link to="/menu"> Menu </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
         </div>
       </div>
       <div className="rightSide">
-        <Link to="/"> Home </Link>
+        <Link to="/home"> Home </Link>
         <Link to="/menu"> Menu </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
